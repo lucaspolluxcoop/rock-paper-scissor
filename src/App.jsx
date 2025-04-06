54efad6fe85976d757287c14151f1a0ce442559f
Lucas <lucas@polluxcoop.com>
2025-04-06 20:34:36 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-gray-800">Piedra, papel o tijera</h1>
+    </>
+  );
+}
+
+export default App;
